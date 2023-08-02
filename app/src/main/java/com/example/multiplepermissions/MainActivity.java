d46b95e57069dba13ab6751d1b9f74fdc150b848
Kieran Ameresekere <kieraniscoolerthanu@gmail.com>
2023-08-02 22:41:07 +0530

    Initial commit

---
+++ app/src/main/java/com/example/multiplepermissions/MainActivity.java
@@ -0,0 +1,59 @@
+package com.example.multiplepermissions;
+
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.app.ActivityCompat;
+
+import android.Manifest;
+import android.content.Context;
+import android.content.pm.PackageManager;
+import android.os.Bundle;
+import android.widget.Toast;
+
+public class MainActivity extends AppCompatActivity {
+    private String[] permissions_mine;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        permissions_mine=new String[]{Manifest.permission.CALL_PHONE};
+
+        if(!hasPermissions(MainActivity.this,permissions_mine)){
+            ActivityCompat.requestPermissions(MainActivity.this,permissions_mine,1);
+        }
+    }
+    private boolean hasPermissions(Context context,String... PERMISSIONS) {
+        if(context != null && PERMISSIONS != null){
+            for(String permission:PERMISSIONS){
+                if(ActivityCompat.checkSelfPermission(context,permission)!= PackageManager.PERMISSION_GRANTED){
+                    return false;
+                }
+            }
+        }
+        return true;
+    }
+    public void onRequestPermissionsResult(int requestCode,String[] permissions,int[] grantResults){
+        super.onRequestPermissionsResult(requestCode, permissions, grantResults);
+        if (requestCode == 1) {
+            if (grantResults[0] == PackageManager.PERMISSION_GRANTED) {
+                Toast.makeText(this, "Calling Permission is granted", Toast.LENGTH_SHORT).show();
+            }else {
+                Toast.makeText(this, "Calling Permission is denied", Toast.LENGTH_SHORT).show();
+            }
+        }
+        if (requestCode == 2) {
+            if (grantResults[0] == PackageManager.PERMISSION_GRANTED) {
+                Toast.makeText(this, "Camera Permission is granted", Toast.LENGTH_SHORT).show();
+            }else {
+                Toast.makeText(this, "Camera Permission is denied", Toast.LENGTH_SHORT).show();
+            }
+        }
+        if (requestCode == 3) {
+            if (grantResults[0] == PackageManager.PERMISSION_GRANTED) {
+                Toast.makeText(this, "SMS Permission is granted", Toast.LENGTH_SHORT).show();
+            }else {
+                Toast.makeText(this, "SMS Permission is denied", Toast.LENGTH_SHORT).show();
+            }
+        }
+    }
+}
